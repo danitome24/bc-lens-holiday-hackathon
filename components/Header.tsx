9a05821e84501d391ae70f419a59cfd79c2a7ec2
danieltomefernandez <danieltomefer@gmail.com>
2024-12-17 16:25:25 +0100

    Included connectkit and web3 provider

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,18 @@
+"use client";
+
+import { ConnectKitButton } from "connectkit";
+
 export const Header = () => {
   return (
     <div className="navbar bg-base-100 text-base-content">
-      <div className="navbar-start">
-        {/* LOGO */}
-      </div>
+      <div className="navbar-start">{/* LOGO */}</div>
       <div className="navbar-center">
-        <a className="btn btn-ghost hover:bg-transparent text-xl">LensSocialScore</a>
+        <a className="btn btn-ghost hover:bg-transparent text-xl">
+          LensSocialScore
+        </a>
       </div>
       <div className="navbar-end">
-        <button className="btn">Connect Lens Account</button>
+        <ConnectKitButton />
       </div>
     </div>
   );
