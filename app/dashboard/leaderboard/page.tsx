095d13871515619694482e74e75f1f68429834d2
danieltomefernandez <danieltomefer@gmail.com>
2024-12-20 07:17:06 +0100

    Add leaderboard page and add links to header

---
+++ app/dashboard/leaderboard/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const LeaderboardPage: NextPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-800">
+      <main className="p-8">
+        <div className="bg-gray-50">LEADERBOARD PAGE</div>
+      </main>
+    </div>
+  );
+};
+
+export default LeaderboardPage;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { ConnectKitButton } from "connectkit";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <div className="navbar bg-base-100 text-base-content">
-      <div className="navbar-start">{/* LOGO */}</div>
+      <div className="navbar-start">
+        <ul className="flex flex-row gap-2">
+          <li>
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link href="/dashboard/leaderboard">Leaderboard</Link>
+          </li>
+        </ul>
+      </div>
       <div className="navbar-center">
         <a className="btn btn-ghost hover:bg-transparent text-xl">
           LensSocialScore
