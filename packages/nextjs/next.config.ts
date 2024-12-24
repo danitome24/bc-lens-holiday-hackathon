625f4ed315e6795d7a142ca90f0833fb047e3c4d
danieltomefernandez <danieltomefer@gmail.com>
2024-12-24 14:44:30 +0100

    Avoid error with pino-pretty

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -19,6 +19,11 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
