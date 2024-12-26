befcc990747128f787c755ea439643b16f988f3b
danieltomefernandez <danieltomefer@gmail.com>
2024-12-26 09:47:56 +0100

    Implement LensScoreSBT contract as ERC721 and add formatting configuration

--- packages/foundry/contracts/LensScoreSBT.sol
+++ packages/foundry/contracts/LensScoreSBT.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-//import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 /**
  * @title LensScoreSBT
  * @dev The LensScoreSBT is a soul bound token that is used to track the score of a lens.
  */
-contract LensScoreSBT { /*is ERC721*/
-    string message = "LensScoreSBT hello world";
-
-    function getMessage() public view returns (string memory) {
-        return message;
+contract LensScoreSBT is ERC721 {
+    struct Score {
+        uint256 score;
+        uint256 timestamp;
     }
+
+    constructor() ERC721("LensScoreSBT", "LSBT") { }
 }
