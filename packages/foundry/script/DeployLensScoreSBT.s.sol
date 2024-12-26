be74762f09f227de76e6ad28067c249761e3269a
danieltomefernandez <danieltomefer@gmail.com>
2024-12-26 11:42:44 +0100

    Formatted script code

--- packages/foundry/script/DeployLensScoreSBT.s.sol
+++ packages/foundry/script/DeployLensScoreSBT.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {Script, console} from "forge-std/Script.sol";
-import {LensScoreSBT} from "../contracts/LensScoreSBT.sol";
+import { Script, console } from "forge-std/Script.sol";
+import { LensScoreSBT } from "../contracts/LensScoreSBT.sol";
 
 contract DeployLensScoreSBT is Script {
     function run() public {
