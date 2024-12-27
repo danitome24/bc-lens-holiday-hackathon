b7f89bffd11392ff008e090e4927127f30d807a2
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-12-27 22:44:25 +0100

    Implement mint NFT functionality with transaction status feedback

--- packages/nextjs/components/MintNFTButton.tsx
+++ packages/nextjs/components/MintNFTButton.tsx
@@ -1,10 +1,37 @@
+import { contractAddress, abi } from "@/abis/LensScoreSBT.info";
 import { Score } from "@/types";
+import { useWriteContract, useWaitForTransactionReceipt } from "wagmi";
 
 type MintNFTButtonProps = {
   walletAddress: string;
   score: Score;
-}
+};
 
-export const MintNFTButton = ({walletAddress, score}: MintNFTButtonProps) => {
-  return <button>MINT NFT</button>;
+export const MintNFTButton = ({ walletAddress, score }: MintNFTButtonProps) => {
+  const { data: hash, isPending, writeContractAsync } = useWriteContract();
+
+  const handleMintNFT = async () => {
+    await writeContractAsync({
+      abi,
+      address: contractAddress,
+      functionName: "mint",
+    });
+  };
+
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    });
+
+  return (
+    <button onClick={() => handleMintNFT()} className="btn btn-secondary">
+      {isPending
+        ? "Minting..."
+        : isConfirming
+        ? "Confirming..."
+        : isConfirmed
+        ? "Minted!"
+        : "Mint NFT"}
+    </button>
+  );
 };
