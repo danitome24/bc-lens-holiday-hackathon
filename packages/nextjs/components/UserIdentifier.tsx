d6908d652c89a022a62caf95a187540b89371e1e
Daniel Tomé Fernández <danieltomefer@gmail.com>
2025-01-02 22:15:53 +0100

    Update UseEffect dependency on UserIdentifier component

--- packages/nextjs/components/UserIdentifier.tsx
+++ packages/nextjs/components/UserIdentifier.tsx
@@ -22,7 +22,7 @@ export const UserIdentifier = ({ walletAddress }: UserIdentifierProps) => {
         `${walletAddress.slice(0, 6)}....${walletAddress.slice(-4)}`
       );
     }
-  }, [data]);
+  }, [data, walletAddress]);
 
   return <p>{lensHandle}</p>;
 };
